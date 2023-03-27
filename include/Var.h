5b8b334d2fde5506a13cd2477aed5fc9fad0c012
TorchTheo <2961620012@qq.com>
2023-03-27 19:18:58 +0800

    2

--- include/Var.h
+++ include/Var.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: TorchTheo 2961620012@qq.com
+ * @Date: 2023-03-27 19:18:37
+ * @LastEditors: TorchTheo 2961620012@qq.com
+ * @LastEditTime: 2023-03-27 19:18:38
+ * @FilePath: /ExpCalc/include/Var.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #pragma once
 #include <string>
 #include "Number.h"
